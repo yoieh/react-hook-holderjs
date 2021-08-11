710bae0aa8457ff0e7ae28c33577f0c800980b3a
Johan Nilsson <yoieh@live.se>
2021-08-11 17:39:08 +0200

    Updates

---
+++ precache-manifest.70139ce378adfe0757a6d0ea83ca6803.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66108a273191cdeb43bf46821e42ac85",
+    "url": "/react-hook-holderjs/index.html"
+  },
+  {
+    "revision": "44c5cd635da2b2512960",
+    "url": "/react-hook-holderjs/static/css/main.2c6a48fe.chunk.css"
+  },
+  {
+    "revision": "dd0f49abe0fcd3cc177d",
+    "url": "/react-hook-holderjs/static/js/2.ecea1f79.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-hook-holderjs/static/js/2.ecea1f79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44c5cd635da2b2512960",
+    "url": "/react-hook-holderjs/static/js/main.0af6edb7.chunk.js"
+  },
+  {
+    "revision": "a32bf14e14e2bb591d7d1ba8fbfaddd1",
+    "url": "/react-hook-holderjs/static/js/main.0af6edb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f96311a933ba3e6c9cc",
+    "url": "/react-hook-holderjs/static/js/runtime-main.88f17b46.js"
+  }
+]);
