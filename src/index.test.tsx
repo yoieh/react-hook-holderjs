d1a8b3b52c8555fad15043b84ead9ebf8a1ff62a
Johan Nilsson <yoieh@live.se>
2021-08-12 11:00:50 +0200

    working test

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -1,8 +1,26 @@
-import { useHolderjs } from "./";
 import { renderHook } from "@testing-library/react-hooks";
 
-describe("ExampleComponent", () => {
+describe("useHolderjs", () => {
   it("retruns nothing", () => {
+    document.body.innerHTML = `
+    <input id="newTodoInput" />
+    <button id="addTodoBtn">Add todo</button>
+    <ol id="todoList"></ol>
+  `;
+
+    HTMLCanvasElement.prototype.getContext = () => {
+      // return whatever getContext has to return
+      const toDataURL = (string: string) =>
+        "data:" +
+        string +
+        ";base64, iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==";
+
+      return {
+        toDataURL
+      };
+    };
+
+    const { useHolderjs } = require("./");
     const { result } = renderHook(() => useHolderjs());
 
     expect(result.current).toBe(undefined);
