0bd9298e0c8339a52d66c2d2103b43813cdeecba
Johan Nilsson <yoieh@live.se>
2021-08-12 11:08:44 +0200

    working test!

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -8,17 +8,10 @@ describe("useHolderjs", () => {
     <ol id="todoList"></ol>
   `;
 
-    HTMLCanvasElement.prototype.getContext = () => {
-      // return whatever getContext has to return
-      const toDataURL = (string: string) =>
-        "data:" +
-        string +
-        ";base64, iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==";
-
-      return {
-        toDataURL
-      };
-    };
+    HTMLCanvasElement.prototype.toDataURL = (string: string) =>
+      "data:" +
+      string +
+      ";base64, iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==";
 
     const { useHolderjs } = require("./");
     const { result } = renderHook(() => useHolderjs());
