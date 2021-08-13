decc1d5aeb6b9c8cfaa57c2526150d8d997394f9
Johan Nilsson <yoieh@live.se>
2021-08-13 13:26:10 +0200

    added more sheld bages

--- example/src/componets/ExamplePage.tsx
+++ example/src/componets/ExamplePage.tsx
@@ -12,7 +12,7 @@ import {
 } from "@mantine/core";
 import { Prism } from "@mantine/prism";
 
-import { Star, Fork, Watch, Sponsor /*, UsedBy*/ } from "react-github-buttons";
+import { Github } from "@uiw/react-shields";
 
 import Example from "./Example";
 import Head from "./Head";
@@ -55,7 +55,7 @@ const ExamplePage = () => {
                         <img
                           src='https://img.shields.io/npm/v/use-holderjs.svg'
                           alt='npm version'
-                          height='18'
+                          height='20'
                         />
                       </a>
                     </div>
@@ -65,16 +65,99 @@ const ExamplePage = () => {
                         <img
                           src='https://app.travis-ci.com/yoieh/use-holderjs.svg?branch=main'
                           alt='build status'
-                          height='18'
+                          height='20'
                         />
                       </a>
                     </div>
 
-                    <Star owner='yoieh' repo='use-holderjs' />
-                    <Fork owner='yoieh' repo='use-holderjs' />
-                    <Watch owner='yoieh' repo='use-holderjs' />
-                    <Sponsor owner='yoieh' repo='use-holderjs' />
-                    {/* <UsedBy owner='yoieh' repo='use-holderjs' count={0} /> */}
+                    <div>
+                      <Github.Issues
+                        user='yoieh'
+                        repo='use-holderjs'
+                        type='issues'
+                      />
+                    </div>
+
+                    <div>
+                      <Github.Issues
+                        user='yoieh'
+                        repo='use-holderjs'
+                        type='issues-closed'
+                      />
+                    </div>
+
+                    <div>
+                      <Github.Issues
+                        user='yoieh'
+                        repo='use-holderjs'
+                        type='issues-pr'
+                      />
+                    </div>
+
+                    <div>
+                      <Github.Issues
+                        user='yoieh'
+                        repo='use-holderjs'
+                        type='issues-pr-closed'
+                      />
+                    </div>
+
+                    <div>
+                      <Github.Size user='yoieh' repo='use-holderjs' />
+                    </div>
+
+                    <div>
+                      <Github.Size
+                        user='yoieh'
+                        repo='use-holderjs'
+                        type='repo-size'
+                      />
+                    </div>
+
+                    <div>
+                      <Github.Analysis
+                        user='yoieh'
+                        repo='use-holderjs'
+                        type='languages-top'
+                      />
+                    </div>
+
+                    <div>
+                      <Github.Analysis
+                        user='yoieh'
+                        repo='use-holderjs'
+                        type='languages-count'
+                      />
+                    </div>
+
+                    <div>
+                      <Github.Social
+                        user='yoieh'
+                        repo='use-holderjs'
+                        type='forks'
+                      />
+                    </div>
+                    <div>
+                      <Github.Social
+                        user='yoieh'
+                        repo='use-holderjs'
+                        type='stars'
+                      />
+                    </div>
+                    <div>
+                      <Github.Social
+                        user='yoieh'
+                        repo='use-holderjs'
+                        type='watchers'
+                      />
+                    </div>
+                    <div>
+                      <Github.Social
+                        user='yoieh'
+                        repo='use-holderjs'
+                        type='followers'
+                      />
+                    </div>
                   </Group>
                 </Col>
               </Grid>
